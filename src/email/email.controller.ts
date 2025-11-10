981ead14a3fe03bee12620869ab9bacfc4ab89e6
goodylove <goodyc474@gmail.com>
2025-11-10 15:27:10 +0100

    feat:  add modules for email logic

---
+++ src/email/email.controller.ts
@@ -0,0 +1,9 @@
+interface EmailJobPayload {
+    request_id: string
+    user_id: string
+    notification_type: string
+    channel_priority: string,
+    message_data: Record<string, string>
+    retry_count: number
+
+}
